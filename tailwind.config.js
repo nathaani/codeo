4ed29ffd38f4b630fc19e94253c6d53687647898
WANWANBUNBUN <144153913+WANWANBUNBUN@users.noreply.github.com>
2024-03-24 16:51:36 -0400

    modify tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./<custom directory>/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
-        'custom-purple': '#3f3cbb',
-        'custom-pink': '#ec4899',
+        "custom-purple": "#3f3cbb",
+        "custom-pink": "#ec4899",
         // Define other custom colors here
       },
       borderRadius: {
-        '4xl': '2rem',
+        "4xl": "2rem",
         // other custom border radius
       },
       fontFamily: {
-        'overpass': ['Overpass', 'sans-serif'],
-        'cabin': ['Cabin', 'sans-serif'],
-        'roboto-mono': ['Roboto Mono', 'monospace'],
+        overpass: ["Overpass", "sans-serif"],
+        cabin: ["Cabin", "sans-serif"],
+        "roboto-mono": ["Roboto Mono", "monospace"],
         // other custom fonts
       },
       // Define other theme extensions here
