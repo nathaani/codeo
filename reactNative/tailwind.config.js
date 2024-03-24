8df4ccc244932c82092095ae72e6da4b50640037
WANWANBUNBUN <144153913+WANWANBUNBUN@users.noreply.github.com>
2024-03-24 17:58:49 -0400

    add nativeWind again, add a folder name Components

---
+++ reactNative/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./Game.js",
+    "./Home.js",
+    "./Level.js",
+    "./Textbook.js",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
