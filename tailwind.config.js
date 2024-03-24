8a7c816a24a1396f3663d1d8dda9f6d52cb3f908
WANWANBUNBUN <144153913+WANWANBUNBUN@users.noreply.github.com>
2024-03-24 17:26:32 -0400

    Revert "modify tailwind config"
    This reverts commit 4ed29ffd38f4b630fc19e94253c6d53687647898.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
-  content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./<custom directory>/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
-        "custom-purple": "#3f3cbb",
-        "custom-pink": "#ec4899",
+        'custom-purple': '#3f3cbb',
+        'custom-pink': '#ec4899',
         // Define other custom colors here
       },
       borderRadius: {
-        "4xl": "2rem",
+        '4xl': '2rem',
         // other custom border radius
       },
       fontFamily: {
-        overpass: ["Overpass", "sans-serif"],
-        cabin: ["Cabin", "sans-serif"],
-        "roboto-mono": ["Roboto Mono", "monospace"],
+        'overpass': ['Overpass', 'sans-serif'],
+        'cabin': ['Cabin', 'sans-serif'],
+        'roboto-mono': ['Roboto Mono', 'monospace'],
         // other custom fonts
       },
       // Define other theme extensions here
