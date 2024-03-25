fca2e0cd48e1a095811e9b157e5dd38d394e9958
nathaani <144153847+nathaani@users.noreply.github.com>
2024-03-25 17:12:59 -0400

    Fixed Wanlin's Native-Base bug by removing Native Base
    - reinstalled node modules with proper plugins
    
    - accidentally removed node modules from parent folder

--- reactNative/Profile.js
+++ reactNative/Profile.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
 import Statistic from "./components/profile/Statistic";
-import Setting from "./components/profile/Setting";
 
 export default function Profile({ navigation }) {
   return (
